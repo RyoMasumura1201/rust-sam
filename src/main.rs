aeed1a1bc0d1493a49cdab3acc2e1fb198c5803b
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2023-03-16 08:16:06 +0900

    不要なコメントアウトを削除

--- src/main.rs
+++ src/main.rs
@@ -9,45 +9,16 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    Init { name: Option<String> },
+    Init {},
 }
-
 
 fn main() {
     let cli = Cli::parse();
 
     match &cli.command {
-        Some(Commands::Init { name }) => {
-            println!("'myapp init' was used, name is: {name:?}")
+        Some(Commands::Init { }) => {
+            println!("'myapp init' was used")
         }
         None => {}
     }
 }
-
-// use clap::{Parser, Subcommand};
-
-// #[derive(Parser)]
-// #[command(author, version, about, long_about = None)]
-// #[command(propagate_version = true)]
-// struct Cli {
-//     #[command(subcommand)]
-//     command: Commands,
-// }
-
-// #[derive(Subcommand)]
-// enum Commands {
-//     /// Adds files to myapp
-//     Add { name: Option<String> },
-// }
-
-// fn main() {
-//     let cli = Cli::parse();
-
-//     // You can check for the existence of subcommands, and if found use their
-//     // matches just as you would the top level cmd
-//     match &cli.command {
-//         Commands::Add { name } => {
-//             println!("'myapp add' was used, name is: {name:?}")
-//         }
-//     }
-// }
