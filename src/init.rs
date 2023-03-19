5a6bc4963efc09008852d5f2003a1186776783cf
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2023-03-20 08:50:40 +0900

    clone先を指定

--- src/init.rs
+++ src/init.rs
@@ -3,10 +3,14 @@ use std::process::Command;
 pub fn clone() {
     const SAM_TEMPLATE_URL: &str = "https://github.com/aws/aws-sam-cli-app-templates.git";
 
+    const REPOSITORY_URL: &str = "aws-sam-cli-app-templates";
+
+    let clone_to_dir = ".".to_string() + REPOSITORY_URL;
+
     println!("Cloning from https://github.com/aws/aws-sam-cli-app-templates (process may take a moment)");
 
     Command::new("git")
-        .args(["clone", SAM_TEMPLATE_URL])
+        .args(["clone", SAM_TEMPLATE_URL, &clone_to_dir])
         .output()
         .expect("failed to clone repository");
 }
