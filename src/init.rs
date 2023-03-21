28f3ca3a9fb7695d8f7403b10521d420864c4457
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2023-03-21 09:11:01 +0900

    cloneでpathbufを使うように

--- src/init.rs
+++ src/init.rs
@@ -1,16 +1,20 @@
 use std::process::Command;
+use std::path::PathBuf;
 
 pub fn clone() {
     const SAM_TEMPLATE_URL: &str = "https://github.com/aws/aws-sam-cli-app-templates.git";
 
-    const REPOSITORY_URL: &str = "aws-sam-cli-app-templates";
+    const REPOSITORY_DIR: &str = ".aws-sam-cli-app-templates";
 
-    let clone_to_dir = ".".to_string() + REPOSITORY_URL;
+    let clone_to_dir = PathBuf::from(".");
+
+    let repo_dir = clone_to_dir.join(REPOSITORY_DIR);
+    let repo_dir_str = repo_dir.as_path().to_str().expect("Failed to convert PathBuf to str");
 
     println!("Cloning from https://github.com/aws/aws-sam-cli-app-templates (process may take a moment)");
 
     Command::new("git")
-        .args(["clone", SAM_TEMPLATE_URL, &clone_to_dir])
+        .args(["clone", SAM_TEMPLATE_URL, repo_dir_str])
         .output()
         .expect("failed to clone repository");
 }
