435e9a490ea5a30c4d21e0c59d2a971fcc4eb193
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2023-04-10 08:25:13 +0900

    cookiecutter関数がResultを返すように

--- src/cookiecutter.rs
+++ src/cookiecutter.rs
@@ -5,12 +5,12 @@ use std::io::Read;
 use std::env;
 
 
-pub fn cookiecutter(template: PathBuf, extra_context: Value){
+pub fn cookiecutter(template: PathBuf, extra_context: Value)-> Result<(), Box<dyn std::error::Error>>{
 
     let context_file = template.join("cookiecutter.json");
     let context_file = context_file.as_path();
 
-    let mut context = generate_context(context_file, extra_context);
+    let mut context = generate_context(context_file, extra_context)?;
 
     if let Some(Value::Object(ref mut cookiecutter_map)) = context.get_mut("cookiecutter"){
         cookiecutter_map.insert("_template".to_string(), json!(template.to_str()));
@@ -18,17 +18,16 @@ pub fn cookiecutter(template: PathBuf, extra_context: Value){
     }
 
     println!("{:?}", context);
+    Ok(())
 }
 
-fn generate_context(context_file: &Path, extra_context: Value)-> Value{
-    let mut file = File::open(context_file)
-        .expect("Failed to open context file");
+fn generate_context(context_file: &Path, extra_context: Value)-> Result<Value, Box<dyn std::error::Error>>{
+    let mut file = File::open(context_file)?;
 
     let mut contents = String::new();
-    file.read_to_string(&mut contents)
-        .expect("Failed to read context file");
+    file.read_to_string(&mut contents)?;
 
-    let mut obj: Value = serde_json::from_str(&contents).expect("faile to create context object");
+    let mut obj: Value = serde_json::from_str(&contents)?;
 
     apply_overwrites_to_context(&mut obj, extra_context);
 
@@ -37,7 +36,7 @@ fn generate_context(context_file: &Path, extra_context: Value)-> Value{
         "cookiecutter": obj
     });
 
-    context
+    Ok(context)
 }
 
 fn apply_overwrites_to_context(context: &mut Value, overwrite_context: Value){
