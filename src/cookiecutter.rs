dd89026f6d2056dd43db39d5a481e7ad9deaa7bc
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2023-04-09 13:35:39 +0900

    contextに_templateフィールドを追加

--- src/cookiecutter.rs
+++ src/cookiecutter.rs
@@ -9,10 +9,16 @@ pub fn cookiecutter(template: PathBuf, extra_context: Value){
     let context_file = template.join("cookiecutter.json");
     let context_file = context_file.as_path();
 
-    generate_context(context_file, extra_context)
+    let mut context = generate_context(context_file, extra_context);
+
+    if let Some(Value::Object(ref mut cookiecutter_map)) = context.get_mut("cookiecutter"){
+        cookiecutter_map.insert("_template".to_string(), json!(template.to_str()));
+    }
+
+    println!("{:?}", context);
 }
 
-fn generate_context(context_file: &Path, extra_context: Value){
+fn generate_context(context_file: &Path, extra_context: Value)-> Value{
     let mut file = File::open(context_file)
         .expect("Failed to open context file");
 
@@ -25,6 +31,11 @@ fn generate_context(context_file: &Path, extra_context: Value){
     apply_overwrites_to_context(&mut obj, extra_context);
 
     println!("{:#?}", obj);
+    let context: Value = json!({
+        "cookiecutter": obj
+    });
+
+    context
 }
 
 fn apply_overwrites_to_context(context: &mut Value, overwrite_context: Value){
