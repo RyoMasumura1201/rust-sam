525ec9f8046167f7fd79ed336f1481883d63fd18
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2023-05-07 12:00:52 +0900

    mainの型を変更

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ pub mod config;
 pub mod cookiecutter;
 mod init;
 use crate::config as global_config;
+use std::error::Error;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     global_config::create_config_directory_if_not_exists()
         .expect("configディレクトリ作成に失敗しました");
     let cli = Cli::parse();
@@ -17,24 +18,16 @@ fn main() {
             match name {
                 Some(name) => {
                     println!("name is {}", name);
-                    match init::init(name) {
-                        Ok(_) => {}
-                        Err(e) => {
-                            println!("init failed {:?}", e);
-                        }
-                    }
+                    init::init(name)?;
                 }
                 None => {
                     println!("name is not given");
-                    match init::init("sam-app") {
-                        Ok(_) => {}
-                        Err(e) => {
-                            println!("init failed {:?}", e);
-                        }
-                    }
+                    init::init("sam-app")?;
                 }
             }
         }
         None => {}
     }
+
+    Ok(())
 }
